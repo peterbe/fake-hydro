aa4c6f86513d52dbdbd2420e9460cacdcfdba687
Peter Bengtsson <mail@peterbe.com>
2023-03-22 09:50:50 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# fake-hydro
+
+Now you can put
+
+```sh
+HYDRO_ENDPOINT=http://localhost:6666/hydro/api/v1/events
+HYDRO_SECRET=blablablablabla
+```
+
+in your `.env` and Hydro sending will be enabled locally, but writes
+to the `db.json` file.
+
+Start it with:
+
+```sh
+npm install
+npm run start
+```
+
+If you want to test the authorization start it like this:
+
+```sh
+HYDRO_SECRET=blablablablabla npm run start
+```
---
+++ index.js
@@ -0,0 +1,90 @@
+import { createHmac } from "crypto";
+
+import express from "express";
+import chalk from "chalk";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
+
+const app = express();
+const PORT = parseInt(process.env.PORT || "6666");
+
+app.use(express.raw({ type: "*/*" }));
+
+const DB_FILE = "db.json";
+const adapter = new JSONFile(DB_FILE);
+const db = new Low(adapter);
+await db.read();
+db.data ||= { events: [] };
+
+app.get("/*", (req, res) => {
+  console.log(req.url);
+  const msg = `GETTING to ${req.url.slice(1)}`;
+  console.log(msg);
+  res.send(msg);
+});
+
+app.post("/*", (req, res) => {
+  const msg = `POSTING to ${req.url.slice(1)}`;
+  const body = req.body.toString("utf-8");
+  const bodyData = JSON.parse(body);
+  const secret = process.env.HYDRO_SECRET;
+  if (secret) {
+    const token = createHmac("sha256", secret).update(req.body).digest("hex");
+    if (req.headers.authorization !== `Hydro ${token}`) {
+      console.warn(
+        chalk.red(`authorization header does not match '${secret}'`)
+      );
+      return res.status(403).send("Bad token");
+    }
+  }
+  console.log("Event incoming!");
+  console.log(bodyData);
+
+  console.log("");
+  for (const [date, counts] of countSchemas(db.data.events)) {
+    console.log(
+      `Counts ${chalk.bold(date)} ${chalk.dim("(delete db.json to reset)")}`
+    );
+    for (const [schema, count] of Object.entries(counts)) {
+      console.log(
+        `  ${chalk.green(schema.padEnd(25))}  ${chalk.yellowBright(
+          `${count}`.padStart(4)
+        )}`
+      );
+    }
+  }
+
+  db.data.events.push({
+    uri: req.url.slice(1),
+    date: new Date(),
+    body: bodyData,
+  });
+
+  db.write().then(() => {
+    console.log("Reported into", DB_FILE);
+  });
+  console.log(msg);
+  res.send(msg);
+});
+
+function countSchemas(events) {
+  const byDay = {};
+  for (const { date, body } of events) {
+    const dateStr =
+      typeof date === "string"
+        ? date.split("T")[0]
+        : date.toISOString().split("T")[0];
+    if (!(dateStr in byDay)) {
+      byDay[dateStr] = {};
+    }
+    const count = byDay[dateStr];
+    for (const { schema } of body.events) {
+      count[schema] = (count[schema] || 0) + 1;
+    }
+  }
+  return Object.entries(byDay).sort((a, b) => a[0].localeCompare(b[0]));
+}
+
+app.listen(PORT, () => {
+  console.log(`Fake Hydro app listening on port ${PORT}`);
+});
