73cacf41434065b69914ced109ad9a6b2f6ad49f
Peter Bengtsson <mail@peterbe.com>
2024-05-17 15:08:16 -0400

    lint

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ if (process.env.HIGHLIGHT) {
   HIGHLIGHT.push(
     ...process.env.HIGHLIGHT.split(",")
       .map((s) => s.trim())
-      .filter(Boolean)
+      .filter(Boolean),
   );
 }
 HIGHLIGHT.push(...process.argv.slice(2));
@@ -27,7 +27,7 @@ function highlightEvents(events) {
   for (const event of events) {
     if (
       HIGHLIGHT.find((highlight) =>
-        event.schema.toLowerCase().includes(highlight.toLowerCase())
+        event.schema.toLowerCase().includes(highlight.toLowerCase()),
       )
     ) {
       return true;
@@ -62,7 +62,7 @@ app.post("/*", (req, res) => {
     const token = createHmac("sha256", secret).update(req.body).digest("hex");
     if (req.headers.authorization !== `Hydro ${token}`) {
       console.warn(
-        chalk.red(`authorization header does not match '${secret}'`)
+        chalk.red(`authorization header does not match '${secret}'`),
       );
       return res.status(403).send("Bad token");
     }
@@ -93,13 +93,13 @@ function printAggregates(events) {
   console.log("");
   for (const [date, counts] of countSchemas(events)) {
     console.log(
-      `Counts ${chalk.bold(date)} ${chalk.dim("(delete db.json to reset)")}`
+      `Counts ${chalk.bold(date)} ${chalk.dim("(delete db.json to reset)")}`,
     );
     for (const [schema, count] of Object.entries(counts)) {
       console.log(
         `  ${chalk.green(schema.padEnd(25))}  ${chalk.yellowBright(
-          `${count}`.padStart(4)
-        )}`
+          `${count}`.padStart(4),
+        )}`,
       );
     }
   }
